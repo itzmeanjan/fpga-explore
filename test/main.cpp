65a128d93522387189405d6ff8776d718e999e44
Anjan Roy <anjanroy@yandex.com>
2022-01-12 07:39:12 +0000

    test method_1 kernel for summation

--- test/main.cpp
+++ test/main.cpp
@@ -16,9 +16,17 @@ main(int argc, char** argv)
             << q->get_device().get_info<sycl::info::device::name>() << std::endl
             << std::endl;
 
-  sycl::cl_ulong ts = test_summation::method_0(*q, IN_LEN, WG_SIZE);
-  std::cout << "passed summation ( method_0 ) test\t\t"
-            << "in " << (double)ts * 1e-6 << " ms" << std::endl;
+  {
+    sycl::cl_ulong ts = test_summation::method_0(*q, IN_LEN, WG_SIZE);
+    std::cout << "passed summation ( method_0 ) test\t\t"
+              << "in " << (double)ts * 1e-6 << " ms" << std::endl;
+  }
+
+  {
+    sycl::cl_ulong ts = test_summation::method_1(*q, IN_LEN, WG_SIZE);
+    std::cout << "passed summation ( method_1 ) test\t\t"
+              << "in " << (double)ts * 1e-6 << " ms" << std::endl;
+  }
 
   return 0;
 }
