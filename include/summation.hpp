5a1563d9111c2081358ee85610f40aa26dcb4837
Anjan Roy <anjanroy@yandex.com>
2022-01-12 12:51:24 +0000

    summation kernel which follows single work-item model

--- include/summation.hpp
+++ include/summation.hpp
@@ -257,4 +257,30 @@ method_3(sycl::queue& q,
 
   return _evts;
 }
+
+sycl::event
+method_4(sycl::queue& q,
+         const sycl::uint* in,
+         size_t in_len,
+         sycl::uint* const out,
+         std::vector<sycl::event> evts)
+{
+  // must supply atleast 2 input elements to sum together
+  assert(in_len >= 2);
+
+  return q.submit([&](sycl::handler& h) {
+    h.depends_on(evts);
+
+    // single work-item kernel
+    h.single_task<class kernelSummationMethod4>([=]() {
+      sycl::uint tmp = 0;
+
+      for (size_t i = 0; i < in_len; i++) {
+        tmp += *(in + i);
+      }
+
+      *out = tmp;
+    });
+  });
+}
 }
