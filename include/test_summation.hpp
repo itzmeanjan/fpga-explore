c717bfb849bafd098b6e8db4506ec3f94e6343eb
Anjan Roy <anjanroy@yandex.com>
2022-01-12 11:32:31 +0000

    testing new summation kernel where no atomic operations are used

--- include/test_summation.hpp
+++ include/test_summation.hpp
@@ -107,4 +107,54 @@ method_1(sycl::queue& q, size_t in_len, size_t wg_size)
 
   return ts;
 }
+
+sycl::cl_ulong
+method_2(sycl::queue& q, size_t in_len, size_t wg_size)
+{
+  const size_t total_work_items = in_len >> 1;
+  const size_t rev_wg_size =
+    wg_size <= total_work_items ? wg_size : total_work_items;
+  const size_t out_len = total_work_items / rev_wg_size;
+
+  sycl::uint* in_h =
+    static_cast<sycl::uint*>(sycl::malloc_host(sizeof(sycl::uint) * in_len, q));
+  sycl::uint* in_d = static_cast<sycl::uint*>(
+    sycl::malloc_device(sizeof(sycl::uint) * in_len, q));
+  sycl::uint* out_h = static_cast<sycl::uint*>(
+    sycl::malloc_host(sizeof(sycl::uint) * out_len, q));
+  sycl::uint* out_d = static_cast<sycl::uint*>(
+    sycl::malloc_device(sizeof(sycl::uint) * out_len, q));
+  sycl::uint* out_final_h =
+    static_cast<sycl::uint*>(sycl::malloc_host(sizeof(sycl::uint), q));
+  sycl::uint* out_final_d =
+    static_cast<sycl::uint*>(sycl::malloc_device(sizeof(sycl::uint), q));
+
+  random_fill(in_h, in_len);
+
+  sycl::event evt_0 = q.memcpy(in_d, in_h, sizeof(sycl::uint) * in_len);
+  std::vector<sycl::event> evts = summation::method_2(
+    q, in_d, in_len, out_d, out_len, out_final_d, wg_size, { evt_0 });
+  sycl::event evt_3 = q.submit([&](sycl::handler& h) {
+    h.depends_on(evts.at(1));
+    h.memcpy(out_final_h, out_final_d, sizeof(sycl::uint));
+  });
+
+  evt_3.wait();
+
+  // device computed result to be compared against this
+  // host computed result
+  sycl::uint out_cmp = 0;
+  seq_sum(in_h, in_len, &out_cmp);
+
+  assert(*out_final_h == out_cmp);
+
+  sycl::cl_ulong ts = time_event(evts.at(0)) + time_event(evts.at(1));
+
+  sycl::free(in_h, q);
+  sycl::free(in_d, q);
+  sycl::free(out_h, q);
+  sycl::free(out_d, q);
+
+  return ts;
+}
 }
