cc4d8c883d84eef7e9824bdaaa53fb2bd8c75b82
Anjan Roy <anjanroy@yandex.com>
2022-01-13 07:00:19 +0000

    removed redundant header file

--- include/summation.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "summation/method_0.hpp"
-#include "summation/method_1.hpp"
-#include "summation/method_2.hpp"
-#include "summation/method_3.hpp"
-#include "summation/method_4.hpp"
