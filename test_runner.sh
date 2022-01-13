75a3511b69343dfe5a650abf3d84c3684f65010b
Anjan Roy <anjanroy@yandex.com>
2022-01-13 07:44:16 +0000

    run test cases for all kernels

---
+++ test_runner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# just to be safe !
+make clean
+
+# summation
+TARGET=cpu TARGET_KERNEL=summation_method_0 make; make clean
+TARGET=cpu TARGET_KERNEL=summation_method_1 make; make clean
+TARGET=cpu TARGET_KERNEL=summation_method_2 make; make clean
+TARGET=cpu TARGET_KERNEL=summation_method_3 make; make clean
+TARGET=cpu TARGET_KERNEL=summation_method_4 make; make clean
+
+# dot product
+TARGET=cpu TARGET_KERNEL=dot_product_method_0 make; make clean
